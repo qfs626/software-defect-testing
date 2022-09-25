f7a8fb3f24cf68899ce77b1d977ece34543d6592
xiezhixian <268022625@qq.com>
2022-09-25 19:21:02 +0800

    后端初始化及部分依赖导入

---
+++ SD-test/src/main/java/com/bjtu/sdtest/SdTestApplication.java
@@ -0,0 +1,13 @@
+package com.bjtu.sdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdTestApplication.class, args);
+    }
+
+}
---
+++ SD-test/src/test/java/com/bjtu/sdtest/SdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjtu.sdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
