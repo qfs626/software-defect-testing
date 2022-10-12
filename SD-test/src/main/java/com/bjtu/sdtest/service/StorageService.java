630d911f096882cb6c98e8e2e31982bf89a870ef
qfs <2488569367@qq.com>
2022-10-12 17:38:31 +0800

    文件上传记录在数据库同步

--- SD-test/src/main/java/com/bjtu/sdtest/service/StorageService.java
+++ SD-test/src/main/java/com/bjtu/sdtest/service/StorageService.java
@@ -1,9 +1,10 @@
 package com.bjtu.sdtest.service;
 
+import com.bjtu.sdtest.pojo.table.Dataset;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface StorageService {
-    String storeFile(MultipartFile file);
+    String storeFile(MultipartFile file, Dataset dataset);
     Resource loadFileAsResource(String fileName);
 }
